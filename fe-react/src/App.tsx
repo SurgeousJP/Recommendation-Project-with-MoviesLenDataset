436666aa233b532ab1417ab7f0843e2f28b67303
4nh3k <21520579@gm.uit.edu.vn>
2023-11-23 08:49:30 +0700

    scoll to top after navigate

--- fe-react/src/App.tsx
+++ fe-react/src/App.tsx
@@ -1,8 +1,14 @@
+import ScrollToTop from './helpers/ScrollToTop';
 import useRouteElement from './useRouteElement';
 
 function App() {
   const routeElement = useRouteElement();
-  return <div className='text-white'>{routeElement}</div>;
+  return (
+    <div className='text-white'>
+      <ScrollToTop />
+      {routeElement}
+    </div>
+  );
 }
 
 export default App;
